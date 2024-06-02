4a938bfd37e1125cc9d6e17666a2786c5f719c3b
meisterjustice <eziefulejustice@gmail.com>
2024-06-02 11:16:14 +0100

    Add useCopy hook for clipboard functionality

---
+++ utils/axios.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "https://api.supermigrate.xyz/v1/",
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // if (error?.response?.status === 401 || error?.response?.status === 403) {
+    // }
+    return Promise.reject(error);
+  }
+);
+
+const setTokenHeader = async (token?: string) => {
+  if (token) {
+    axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
+  } else {
+    delete axiosInstance.defaults.headers.common.Authorization;
+  }
+};
+
+export { axiosInstance, setTokenHeader };
