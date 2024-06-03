b59ce4645880842da757d0127971a3f1f2dc960b
Favour Nwajoko <chocorazzee@gmail.com>
2024-06-03 15:52:33 +0100

    feat: Add LBTokenCard component for displaying token information in a card format

---
+++ components/token-card/index.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+import React from "react";
+import { SmallBaseIcon } from "@/public/icons";
+import LBClickAnimation from "../click-animation";
+import useTimeAgo from "@/hooks/useTimeAgo";
+import { ILBTokenCard } from "./types";
+
+const LBTokenCard = ({
+  createdAt,
+  name,
+  symbol,
+  id,
+  network,
+}: ILBTokenCard) => {
+  const timeAgo = useTimeAgo();
+  return (
+    <Link href={`/${network}/tokens/new?id=${id}`}>
+      <LBClickAnimation className="p-5 bg-white rounded-lg border border-primary-50 flex flex-col gap-4 w-full h-[275px]">
+        <div className="flex items-center justify-between self-stretch">
+          <div className="w-12 h-12 rounded bg-primary-300" />
+          <SmallBaseIcon />
+        </div>
+
+        <div className="flex flex-col items-start gap-0.5">
+          <div className="flex items-center justify-center gap-1 text-primary-700 text-[20px] leading-[24px]">
+            {name}{" "}
+            <span className="border-[0.5px] border-primary-50 rounded-lg bg-primary-800 px-2 text-primary-750 text-[12px] leading-[24px] font-medium">
+              {symbol}
+            </span>
+          </div>
+          <p className="text-primary-850 text-[14px] leading-[24px]">
+            Deployed {timeAgo(createdAt)}
+          </p>
+        </div>
+      </LBClickAnimation>
+    </Link>
+  );
+};
+
+export default LBTokenCard;
---
+++ components/token-card/types.ts
@@ -0,0 +1,12 @@
+import { Network } from "../button/types";
+
+interface ILBTokenCard {
+  id: string;
+  name: string;
+  symbol: string;
+  createdAt: string;
+  updatedAt: string;
+  network: Network;
+}
+
+export type { ILBTokenCard };
