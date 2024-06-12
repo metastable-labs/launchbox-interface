1ea3173d20b2790c9c69f0c41510af1feb887903
Favour Nwajoko <chocorazzee@gmail.com>
2024-06-12 01:55:11 +0100

    refactor: Remove redundant files and code

--- app/[network]/tokens/page.tsx
+++
@@ -1,8 +0,0 @@
-import { TokensView } from '@/views';
-import { PageProps } from '@/app/type';
-
-const Tokens = ({ params }: PageProps) => {
-  return <TokensView network={params.network} />;
-};
-
-export default Tokens;
