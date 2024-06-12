8c5337b90a02d25ddbcdcaab65ae7253505c6f12
Favour Nwajoko <chocorazzee@gmail.com>
2024-06-12 01:55:11 +0100

    refactor: Remove redundant files and code

--- app/[network]/tokens/new/page.tsx
+++
@@ -1,8 +0,0 @@
-import { NewTokenView } from "@/views";
-import { PageProps } from "@/app/type";
-
-const NewTokens = ({ params }: PageProps) => {
-  return <NewTokenView network={params.network} />;
-};
-
-export default NewTokens;
--- app/type.ts
+++
@@ -1,8 +0,0 @@
-import { Network } from "@/components/button/types";
-
-interface PageProps {
-  params: { network: Network };
-  children?: React.ReactNode;
-}
-
-export type { PageProps };
