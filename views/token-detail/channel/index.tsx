a4995b709727dadb5db1a1ebd42b9994e05d7bfa
Favour Nwajoko <chocorazzee@gmail.com>
2024-06-18 18:14:07 +0100

    refactor: Update token-detail types and components

--- views/token-detail/channel/index.tsx
+++ views/token-detail/channel/index.tsx
@@ -2,21 +2,70 @@ import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import classNames from 'classnames';
 import { AnimatePresence, motion } from 'framer-motion';
+import Rating from 'react-rating-stars-component';
 
-import { BaseBadgeicon, CheckAltIcon, ConfigSiteIcon, CopyIcon, FarcasterIcon, SmallFarcasterIcon, WebIcon } from '@/public/icons';
 import useSystemFunctions from '@/hooks/useSystemFunctions';
-import { LBClickAnimation, LBShare } from '@/components';
 import useCopy from '@/hooks/useCopy';
+import { LBClickAnimation, LBShare } from '@/components';
+import { formatNumber } from '@/components/table/row';
+import { BaseBadgeicon, CheckAltIcon, ConfigSiteIcon, CopyIcon, FarcasterIcon, SmallFarcasterIcon, WebIcon } from '@/public/icons';
 import { Period } from '../types';
-import IChannel from './types';
+import { IChannel, IInfo } from './types';
 import { generateData, periods } from '../dummy';
 import LineChart from '../line-chart';
+
+const dollarRate = 0.014728;
+
+const Info = ({ text, title, activeFollowersPercentage, priceChangePercentage, socialScore, txns, weeklyCastPercentage, hasBorder }: IInfo) => {
+  const primary = activeFollowersPercentage || weeklyCastPercentage || priceChangePercentage;
+  const isPositive = priceChangePercentage && priceChangePercentage > 0;
+  return (
+    <div className={classNames('self-stretch flex items-center justify-between', { 'pb-4 border-b border-b-primary-50': hasBorder })}>
+      <span className="text-primary-700 text-[14px] leading-[24px] ">{title}</span>
+
+      <div className={classNames('text-primary-650 text-[16px] leading-[20px] font-medium uppercase', { 'flex items-center gap-1.5': primary, 'flex items-center justify-center gap-2': socialScore })}>
+        {text && text}
+
+        {primary && (
+          <span
+            className={classNames('text-[12px] leading-[20px] lowercase', {
+              'text-primary-750': activeFollowersPercentage,
+              'text-primary-450': weeklyCastPercentage || (priceChangePercentage && isPositive),
+              'text-primary-1050': priceChangePercentage && !isPositive,
+            })}>
+            {isPositive && '+'}
+            {primary}%{activeFollowersPercentage && ' active'}
+          </span>
+        )}
+
+        {txns && (
+          <div className="flex items-center justify-center gap-1 text-primary-650 text-[16px] leading-[20px] font-medium">
+            <span>{txns.numerator}</span>
+            <span>(</span>
+            <span className="text-primary-2600">{txns.denominator.numerator}</span>
+            <span className="text-primary-750 text-[12px]">/</span>
+            <span className="text-primary-2650">{txns.denominator.denominator}</span>
+            <span>)</span>
+          </div>
+        )}
+
+        {socialScore && (
+          <>
+            <span>(</span>
+            <Rating count={5} value={socialScore} size={16} activeColor="#F2AE40" color="#D9D9D9" isHalf={true} edit={false} classNames="star-ratings" />
+            <span>)</span>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
 const Channel = ({ token, userRole }: IChannel) => {
   const { navigate } = useSystemFunctions();
   const { handleCopy, hasCopied } = useCopy();
 
-  const [period, setPeriod] = useState<Period>('1m');
+  const [period, setPeriod] = useState<Period>('1h');
   const [growthData, setGrowthData] = useState(generateData(period, true));
 
   const noGrowthData = growthData.every((dataPoint) => dataPoint.value === 0);
@@ -43,6 +92,25 @@ const Channel = ({ token, userRole }: IChannel) => {
       onClick: () => window.open(token?.warpcast_channel_link, '_blank'),
       show: userRole === 'user',
     },
+  ];
+
+  const holders = [
+    { name: 'drippy.eth', amount: 95100000, avatarURL: 'https://res.cloudinary.com/dxnd4k222/image/upload/fl_preserve_transparency/v1718723895/Avatar_khczwg.jpg' },
+    { name: 'Evans', amount: 25700000, avatarURL: 'https://res.cloudinary.com/dxnd4k222/image/upload/fl_preserve_transparency/v1718723894/Avatar_1_rg5wgw.jpg' },
+    { name: 'CJ', amount: 7510000, avatarURL: 'https://res.cloudinary.com/dxnd4k222/image/upload/fl_preserve_transparency/v1718723894/Avatar_2_ch4dwg.jpg' },
+    { name: 'tammmy', amount: 95100000, avatarURL: 'https://res.cloudinary.com/dxnd4k222/image/upload/fl_preserve_transparency/v1718723894/Avatar_3_jhomgx.jpg' },
+    { name: 'rossnothere', amount: 64000000, avatarURL: 'https://res.cloudinary.com/dxnd4k222/image/upload/fl_preserve_transparency/v1718723894/Avatar_4_dlvjwk.jpg' },
+  ];
+
+  const info = [
+    { title: 'Channel followers', text: (56801).toLocaleString(), activeFollowersPercentage: 13.3 },
+    { title: 'Weekly cast', text: (10354).toLocaleString(), weeklyCastPercentage: 16.7 },
+    { title: 'Social score', text: (504.01).toLocaleString(), socialScore: 3 },
+    { title: 'Price (1D)', text: `$${0.00567}`, priceChangePercentage: 6.7 },
+    { title: 'Txns', txns: { numerator: 706, denominator: { numerator: 406, denominator: 300 } } },
+    { title: 'Total supply', text: `${token?.token_total_supply.toLocaleString()} ${token?.token_symbol}` },
+    { title: 'Liquidity', text: `$${formatNumber(206_000)}` },
+    { title: 'Volume', text: `$${formatNumber(4_000_000)}` },
   ];
 
   useEffect(() => {
@@ -99,7 +167,7 @@ const Channel = ({ token, userRole }: IChannel) => {
         </div>
 
         <div className="self-stretch flex flex-col items-center gap-4">
-          <div className="self-stretch flex flex-col gap-9 items-center">
+          <div className="self-stretch flex flex-col gap-9 items-center pb-4 border-b border-b-primary-50">
             <div className="self-stretch flex flex-col gap-[30px]">
               <div className="self-stretch flex items-center justify-between">
                 <span className="text-primary-700 text-[14px] leading-[24px]">Channel growth</span>
@@ -119,7 +187,7 @@ const Channel = ({ token, userRole }: IChannel) => {
                 </div>
               </div>
 
-              <div className="w-full flex items-center justify-center h-[295px] relative">
+              <div className="w-full flex items-center justify-center min-h-[180px] relative">
                 <LineChart period={period} data={growthData} variant="secondary" />
 
                 {noGrowthData && (
@@ -137,7 +205,29 @@ const Channel = ({ token, userRole }: IChannel) => {
             </div>
           </div>
 
-          <div></div>
+          <div className="self-stretch flex items-center justify-between">
+            <span className="text-primary-700 text-[14px] leading-[24px]">Holders</span>
+
+            <span className="text-primary-650 leading-[20.8px] font-medium">{(37602).toLocaleString()}</span>
+          </div>
+
+          <div className="flex content-start gap-2.5 flex-wrap pb-4 border-b border-b-primary-50">
+            {holders.map(({ name, amount, avatarURL }, index) => (
+              <div key={index} className="flex items-center gap-0.5 py-1 pl-1 pr-2 justify-center bg-primary-200 rounded-full">
+                <Image src={avatarURL} alt="avatar" width={500} height={500} className="w-4 h-4 object-cover rounded-full" />
+                <p className="font-medium">
+                  <span className="text-primary-250 text-[14px] leading-[21px]">{name}</span>{' '}
+                  <span className="text-[12px] leading-[150%] text-primary-850">
+                    {formatNumber(amount)} = ${formatNumber(amount * dollarRate)}
+                  </span>
+                </p>
+              </div>
+            ))}
+          </div>
+
+          {info.map((item, index) => (
+            <Info key={index} {...item} hasBorder={index !== info.length - 1} />
+          ))}
         </div>
       </div>
     </div>
