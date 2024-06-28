aedb5d1db94a02e6ffbd2c5b03e7e7e77a0819c4
Favour Nwajoko <chocorazzee@gmail.com>
2024-06-28 21:27:35 +0100

    chore: add invalid amount guard for buy or selling token

--- components/trade-interface/index.tsx
+++ components/trade-interface/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import Image from 'next/image';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useConnectModal } from '@rainbow-me/rainbowkit';
 import { useAccount } from 'wagmi';
 import { NumericFormat, OnValueChange } from 'react-number-format';
@@ -11,7 +12,7 @@ import { LBButton, LBClickAnimation } from '@/components';
 import { ILBTradeInterface } from './types';
 import useTokenActions from '@/store/token/actions';
 import useSystemFunctions from '@/hooks/useSystemFunctions';
-import { ETHIcon } from '@/public/icons';
+import { ETHIcon, InfoIcon } from '@/public/icons';
 import { wagmiConfig } from '@/config/rainbow/rainbowkit';
 
 const tabs = ['buy', 'sell'];
@@ -34,6 +35,8 @@ const LBTradeInterface = ({ token, standAlone = true }: ILBTradeInterface) => {
   const [tokenBalance, setTokenBalance] = useState<number>(0);
 
   const balance = tab === 'buy' ? ethBalance : tokenBalance;
+
+  const invalidAmount = (tab === 'buy' && Number(amount) > ethBalance) || (tab === 'sell' && Number(amount) > tokenBalance);
 
   const balancePartitions = [
     { text: '10%', onClick: () => setAmount(truncateToDecimals(balance * 0.1)) },
@@ -98,69 +101,87 @@ const LBTradeInterface = ({ token, standAlone = true }: ILBTradeInterface) => {
   }, [address]);
 
   return (
-    <form onSubmit={onSubmit} className={classNames('h-fit flex flex-col gap-6', { 'p-6 rounded-base border border-primary-50 bg-white': standAlone, 'max-w-80 md:max-w-[343px]': !standAlone })}>
-      <div className="p-1 self-stretch flex gap-1 bg-primary-2700 rounded-xl">
-        {tabs.map((text) => (
-          <div
-            key={text}
-            className={classNames(
-              'p-1 flex-1 flex items-center justify-center rounded-lg text-[15px] leading-[24px] tracking-[-0.075px] font-semibold cursor-pointer transition-colors duration-300 capitalize',
-              {
-                'text-primary-2800 rounded-lg bg-primary-2850 shadow-trade-tab': text === tab,
-                'text-primary-2750': text !== tab,
-              },
-            )}
-            onClick={() => setTab(text as 'buy' | 'sell')}>
-            {text}
-          </div>
-        ))}
-      </div>
-
-      <div className="self-stretch flex flex-col gap-3.5">
-        <div className="p-4 self-stretch flex items-center justify-between bg-primary-2500 rounded-base border border-primary-1200">
-          <NumericFormat
-            value={amount}
-            className="bg-transparent outline-none text-primary-150 text-[24px] leading-[34.8px] max-w-[50%] font-medium"
-            placeholder="Amount"
-            thousandSeparator=","
-            allowNegative={false}
-            onValueChange={handleAmountChange}
-          />
-
-          <div className="flex flex-col items-end justify-center gap-1.5">
-            <div className="px-3 py-1.5 bg-white flex items-center justify-between gap-3 rounded-base">
-              {tokenLogo}
-              <span className="text-primary-2800 font-semibold text-[15px] leading-[24px] tracking-[-0.075px]">{tokenSymbol}</span>
-            </div>
-
-            <p className="text-primary-250 text-[12px] leading-[17.4px] font-medium whitespace-nowrap">
-              Balance: {balance.toLocaleString('en', { maximumFractionDigits: 5 })} {tokenSymbol}
-            </p>
-          </div>
-        </div>
-
-        <div className="self-stretch flex-wrap flex items-center content-center gap-[9px]">
-          {balancePartitions.map(({ text, onClick }, index) => (
-            <LBClickAnimation key={index} className="px-[5px] py-1 bg-primary-200 text-primary-250 font-medium text-[12px] leading-[18.6px] rounded-[3px] flex-1 text-center" onClick={onClick}>
+    <>
+      <form onSubmit={onSubmit} className={classNames('h-fit flex flex-col gap-6', { 'p-6 rounded-base border border-primary-50 bg-white': standAlone, 'max-w-80 md:max-w-[343px]': !standAlone })}>
+        <div className="p-1 self-stretch flex gap-1 bg-primary-2700 rounded-xl">
+          {tabs.map((text) => (
+            <div
+              key={text}
+              className={classNames(
+                'p-1 flex-1 flex items-center justify-center rounded-lg text-[15px] leading-[24px] tracking-[-0.075px] font-semibold cursor-pointer transition-colors duration-300 capitalize',
+                {
+                  'text-primary-2800 rounded-lg bg-primary-2850 shadow-trade-tab': text === tab,
+                  'text-primary-2750': text !== tab,
+                },
+              )}
+              onClick={() => setTab(text as 'buy' | 'sell')}>
               {text}
-            </LBClickAnimation>
-          ))}
-        </div>
-      </div>
-
-      {valueToGet && (
-        <div className="py-3 flex flex-col gap-3 items-stretch rounded-lg">
-          {info.map(({ title, value }, index) => (
-            <div key={index} className="flex items-center justify-between gap-2 text-primary-250 text-sm">
-              <span>{title}</span>
-              <span>{value}</span>
             </div>
           ))}
         </div>
-      )}
 
-      <LBButton loading={tokenState.loadingBuy} disabled={!amount} variant="plainAlt" text={tab} type="submit" tradeType={tab} />
-    </form>
+        <div className="self-stretch flex flex-col gap-3.5">
+          <div className="p-4 self-stretch flex items-center justify-between bg-primary-2500 rounded-base border border-primary-1200">
+            <NumericFormat
+              value={amount}
+              className="bg-transparent outline-none text-primary-150 text-[24px] leading-[34.8px] max-w-[50%] font-medium"
+              placeholder="Amount"
+              thousandSeparator=","
+              allowNegative={false}
+              onValueChange={handleAmountChange}
+            />
+
+            <div className="flex flex-col items-end justify-center gap-1.5">
+              <div className="px-3 py-1.5 bg-white flex items-center justify-between gap-3 rounded-base">
+                {tokenLogo}
+                <span className="text-primary-2800 font-semibold text-[15px] leading-[24px] tracking-[-0.075px]">{tokenSymbol}</span>
+              </div>
+
+              <p className="text-primary-250 text-[12px] leading-[17.4px] font-medium whitespace-nowrap">
+                Balance: {balance.toLocaleString('en', { maximumFractionDigits: 5 })} {tokenSymbol}
+              </p>
+            </div>
+          </div>
+
+          <div className="self-stretch flex-wrap flex items-center content-center gap-[9px]">
+            {balancePartitions.map(({ text, onClick }, index) => (
+              <LBClickAnimation key={index} className="px-[5px] py-1 bg-primary-200 text-primary-250 font-medium text-[12px] leading-[18.6px] rounded-[3px] flex-1 text-center" onClick={onClick}>
+                {text}
+              </LBClickAnimation>
+            ))}
+          </div>
+        </div>
+
+        {valueToGet && (
+          <div className="py-3 flex flex-col gap-3 items-stretch rounded-lg">
+            {info.map(({ title, value }, index) => (
+              <div key={index} className="flex items-center justify-between gap-2 text-primary-250 text-sm">
+                <span>{title}</span>
+                <span>{value}</span>
+              </div>
+            ))}
+          </div>
+        )}
+
+        <LBButton loading={tokenState.loadingBuy} disabled={!amount || invalidAmount} variant="plainAlt" text={tab} type="submit" tradeType={tab} />
+      </form>
+
+      <AnimatePresence>
+        {invalidAmount && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={classNames('mt-6 bg-primary-2950 p-2 flex items-center rounded-[10px] w-full', { 'absolute left-0 top-[96%]': !standAlone })}>
+            <div className="flex items-center justify-center p-1">
+              <InfoIcon width={24} height={24} color="#6E330C" />
+            </div>
+
+            <p className="text-primary-3000 text-[14px] leading-[24px] max-w-[348.7px]">{`You don't have enough ${tab === 'buy' ? 'ETH' : token.token_symbol}`}</p>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
