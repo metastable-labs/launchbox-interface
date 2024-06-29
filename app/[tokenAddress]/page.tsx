badedbe38ea18b5b50f5e15cfafe751a06f5ebd4
meisterjustice <eziefulejustice@gmail.com>
2024-06-29 18:36:09 +0100

    fix build issue

--- app/[tokenAddress]/page.tsx
+++ app/[tokenAddress]/page.tsx
@@ -1,5 +1,4 @@
 import { TokenDetailsView } from '@/views';
-import { PageProps } from '../types';
 
 const TokenDetail = ({ params: { tokenAddress } }: PageProps) => {
   return <TokenDetailsView tokenAddress={tokenAddress} />;
--- app/types.ts
+++ app/types.ts
@@ -1,8 +1,4 @@
-import { Network } from '@/components/button/types';
-
 interface PageProps {
-  params: { network: Network; tokenAddress: string };
+  params: { tokenAddress: string };
   children?: React.ReactNode;
 }
-
-export type { PageProps };
